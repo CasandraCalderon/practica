e253e92f28a69bb2d7484ef18f868c48a032169d
casandra <angie200896@gmail.com>
2021-04-04 20:10:33 -0400

    problem3

--- problemas.js
+++ problemas.js
@@ -1,5 +1,10 @@
 var oracion;
-oracion = prompt("Ingrese frase u oracion que quiera invertir:", "");
+oracion = prompt("Ingrese frase u oracion para comprobar si es palindromo:", "");
 var cont = oracion.length;
-for (var i = cont - 1; i >= 0; i--)
-    document.write(oracion.charAt(i));
+var pal = oracion.charAt(cont - 1);
+for (var i = cont - 2; i >= 0; i--)
+    pal = pal + oracion.charAt(i);
+if (oracion == pal)
+    document.write(true + "");
+else
+    document.write(false + "");
