cc7b0807e583ecc141af71a26af1b79512a9fd11
casandra <angie200896@gmail.com>
2021-04-04 22:13:03 -0400

    problem8

--- problemas.js
+++ problemas.js
@@ -1,17 +1,17 @@
-var cadena1;
-var cadena2;
-var red = cadena1;
-var contador = 0;
-var cad2 = 0;
-cadena1 = prompt("Ingresar cadena 1:", "");
-cadena2 = prompt("Ingresar cadena 2:", "");
-for (var i = 0; i <= cadena1.length - 1; i++) {
-    for (var j = 0; j <= cadena2.length - 1; j++) {
-        if (cadena1.charAt(i) == cadena2.charAt(j)) {
-            contador++;
-            cadena2 = cadena2.replace(cadena2.charAt(j));
-            break;
+var n;
+n = prompt("Ingresar numero:", "");
+var c = 1;
+var p = 2;
+var d = 2;
+while (c <= n) {
+    if (p % d == 0) {
+        if (p == d) {
+            document.write(p + ", ");
+            c++;
         }
+        d = 2;
+        p++;
     }
+    else
+        d++;
 }
-document.write(contador.toString());
--- problemas.ts
+++ problemas.ts
@@ -1,17 +1,17 @@
-var cadena1: string;
-var cadena2: any;
-var red=cadena1;
-var contador: number=0;
-var cad2: number=0;
-cadena1 = prompt("Ingresar cadena 1:", "");
-cadena2 = prompt("Ingresar cadena 2:", "");
-for(var i=0; i<=cadena1.length-1; i++){
-  for (var j=0; j<=cadena2.length-1; j++){
-    if(cadena1.charAt(i)==cadena2.charAt(j)){
-      contador++;
-      cadena2=cadena2.replace(cadena2.charAt(j));
-      break;
-    }
-  }
+var n: any;
+n = prompt("Ingresar numero:", "");
+var c: number= 1;
+var p: number= 2;
+var d: number= 2;
+while (c <= n) {
+   if (p % d == 0) {
+      if (p == d) {
+         document.write(p + ", ");
+         c++;
+      }
+      d = 2;
+      p++;
+   }
+   else
+      d++;
 }
-document.write(contador.toString());
